cac4eb1fc06523d8e1280aa698dc7da7c3b62817
brayanleonardo67 <00001159059159sp@al.educacao.sp.gov.br>
2026-05-29 09:25:37 -0300

    login quase pronto!

--- Front/wms/pages/login/script.js
+++ Front/wms/pages/login/script.js
@@ -1,7 +1,7 @@
 const formulario = document.getElementById('loginForm');
 const mensagem = document.getElementById('mensagem');
 
-formulario.addEventListener('submit', function(evento) {
+formulario.addEventListener('submit', function (evento) {
     evento.preventDefault(); // Evita que a página recarregue ao enviar
 
     const email = document.getElementById('email').value;
@@ -11,11 +11,44 @@ formulario.addEventListener('submit', function(evento) {
     if (email === "usuario@exemplo.com" && senha === "123456") {
         mensagem.style.color = "green";
         mensagem.textContent = "Login realizado com sucesso!";
-        
+
         // Exemplo de redirecionamento:
-        // window.location.href = "pagina-inicial.html";
+        window.location.href = "/Front/wms/pages/home/index.html";
     } else {
         mensagem.style.color = "red";
         mensagem.textContent = "E-mail ou senha incorretos!";
     }
 });
+
+function login(){
+    location.reload();
+}
+
+let contas = []
+let divConta = document.getElementById('newAccount')
+
+function createAccount() {
+    divConta.innerHTML = `
+    <div>
+        <h2>criar uma nova conta</h2>
+                <form id="loginForm">
+                    <div class="input-group">
+                        <label for="text">Nome de usúario</label>
+                        <input type="text" id="nameUser" required>
+                    </div>
+                    <div class="input-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" required>
+                    </div>
+                    <div class="input-group">
+                        <label for="password">Senha</label>
+                        <input type="password" id="password" required>
+                    </div>
+                    <button type="submit">Entrar</button>
+                    <button onClick="login()">Login</button>
+                    <p id="mensagem"></p>
+    </div>
+    `
+
+}
+
